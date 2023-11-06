113f3927c0e01514213b48d218303c85c2a8bf8b
Akim-collab <kalenyuk.akim@mail.ru>
2023-11-07 01:18:13 +0300

    add example code

---
+++ example.py
@@ -0,0 +1,72 @@
+import random
+
+# Simulated forex exchange rates (EUR/USD and GBP/USD)
+eur_usd_rates = [1.10, 1.12, 1.11, 1.13, 1.09]
+gbp_usd_rates = [1.25, 1.24, 1.26, 1.27, 1.23]
+
+# Simulated bid-ask spreads for EUR/USD and GBP/USD
+eur_usd_spread = 0.01
+gbp_usd_spread = 0.02
+
+# Simulated portfolio balances
+initial_balance_usd = 10000
+initial_balance_eur = 0
+initial_balance_gbp = 0
+
+
+
+def assess_liquidity():
+    """
+    Function to assess liquidity risk
+    """
+    # Simulate liquidity risk by randomly determining liquidity status
+    return random.choice(['Low', 'Medium', 'High'])
+
+
+# Function to identify arbitrage opportunities
+def find_arbitrage_opportunity():
+    """
+    Function to identify arbitrage opportunities
+    """
+    eur_usd_bid = eur_usd_rates[0]
+    eur_usd_ask = eur_usd_rates[0] + eur_usd_spread
+    gbp_usd_bid = gbp_usd_rates[0]
+    gbp_usd_ask = gbp_usd_rates[0] + gbp_usd_spread
+
+    # Calculate the cross rates
+    eur_gbp_bid = eur_usd_bid / gbp_usd_ask
+    eur_gbp_ask = eur_usd_ask / gbp_usd_bid
+
+    if eur_gbp_ask < eur_gbp_bid:
+        return True, eur_gbp_bid, eur_gbp_ask
+    else:
+        return False, 0, 0
+
+
+
+def main():
+    """
+    Main function to execute arbitrage strategy
+    """
+    print("Arbitrage Strategy with Liquidity Risk Assessment")
+
+    while True:
+        liquidity = assess_liquidity()
+        arbitrage, eur_gbp_bid, eur_gbp_ask = find_arbitrage_opportunity()
+
+        if arbitrage and liquidity != 'Low':
+            print(
+                f"Arbitrage Opportunity Found! EUR/GBP Buy: {eur_gbp_bid}, Sell: {eur_gbp_ask}, Liquidity: {liquidity}")
+            # Execute the arbitrage by buying low and selling high
+            balance_usd = initial_balance_usd + initial_balance_eur * eur_gbp_ask + initial_balance_gbp * eur_gbp_ask
+            balance_eur = 0
+            balance_gbp = 0
+            print(f"New Balance USD: {balance_usd}, EUR: {balance_eur}, GBP: {balance_gbp}")
+        else:
+            print(f"No arbitrage opportunity or low liquidity. Liquidity: {liquidity}")
+
+        input("Press Enter to continue...")
+
+
+if __name__ == "__main__":
+    main()
